f64d2763ff7cd0960dfb29a69c19c06b7ef224f6
David Aguilar <ag.david086@gmail.com>
2025-12-01 17:36:16 -0400

    inicio de proyecto - primer commit

---
+++ src/components/ListaDeTareas.jsx
@@ -0,0 +1,36 @@
+import TareaFormulario from "./TareaFormulario"
+import '../styles/ListaDeTareas.css'
+import React, { useState } from 'react';
+import Tarea from "./Tarea";
+
+const ListaDeTareas = () => {
+  
+  const [tareas, setTareas] = useState ([]);
+  
+  const agregarTarea = (tarea) => {
+
+    if(tarea.texto.trim()){
+      tarea.texto = tarea.texto.trim();
+      const tareasActualizadas = [tarea, ...tareas]
+      setTareas(tareasActualizadas); 
+    }
+  } 
+
+  return (
+    <>
+      <TareaFormulario  onSubmit={agregarTarea}/>
+      <div className="tareas-lista-contenedor">
+        {
+          tareas.map((tarea) =>  
+            <Tarea 
+              texto={tarea.texto}
+              completada={tarea.completada}
+            />
+          )    
+        }
+      </div>
+    </>
+  ) 
+}
+
+export default ListaDeTareas
